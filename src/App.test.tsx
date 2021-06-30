37893fe3e2b3e2d2a7db07907f0df4e208443ede
tjaartvanderWalt <11085642+tjaartvanderWalt@users.noreply.github.com>
2021-06-30 16:14:08 -0400

    update readme and mention enzyme

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+// you can also use import { mount, shallow } from 'enzyme'; if you want to use enzyme instead of react testing library
 import App from "./App";
 
 test("renders learn react link", () => {
