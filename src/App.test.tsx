0cc86091efaeae1eadbe7e6d94d9a9c44b1edf15
tjaartvanderWalt <11085642+tjaartvanderWalt@users.noreply.github.com>
2021-06-29 21:50:51 -0400

    update app component

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(
+    "Your local Wave UX Challenge is running! Edit src/App.tsx and save to reload."
+  );
   expect(linkElement).toBeInTheDocument();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import React from "react";
 function App() {
   return (
     <p>
-      Your local Wave UX Challenge is running! Edit <code>src/App.tsx</code> and
-      save to reload.
+      Your local Wave UX Challenge is running! Edit src/App.tsx and save to
+      reload.
     </p>
   );
 }
