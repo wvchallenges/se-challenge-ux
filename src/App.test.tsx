7c07c681732b61f89e7cd629a7a6eac13ca2ef67
wvchallenges <admin@waveapps.com>
2021-06-30 16:25:22 -0400

    Draft 1 (#1)

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+// you can also use import { mount, shallow } from 'enzyme'; if you want to use enzyme instead of react testing library
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders <App /> component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const targetElement = screen.getByText(
+    "Your local Wave UX Challenge is running! Edit src/App.tsx and save to reload."
+  );
+  expect(targetElement).toBeInTheDocument();
 });
